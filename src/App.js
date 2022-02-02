c6902ab8fa932499344a5e70ad51c9e533d66da8
Victor Caruso <vic.caruso@gmail.com>
2022-02-02 14:55:13 -0800

    started InstructionsForm component, but not finished

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Dropdown from './Dropdown';
 import OrderNameInput from './OrderNameInput';
 import OrderImages from './OrderImages';
 import InstructionList from './InstructionList';
+import InstructionsForm from './InstructionsForm';
 
 function App() {
   const [foodId, setFoodId] = useState(0);
@@ -22,12 +23,15 @@ function App() {
         Order for {orderName}
       </header>
       <main>
-        <Dropdown setId={setFoodId} options={foodItems}/>
-        <Dropdown setId={setSideId} options={sideItems}/>
-        <Dropdown setId={setDrinkId} options={drinkItems}/>
-        <OrderNameInput setOrderName={setOrderName}/>
-        <OrderImages foodId={foodId} sideId={sideId} drinkId={drinkId}/>
-        <InstructionList instructions={instructions}/>
+        <Dropdown setId={setFoodId} options={foodItems} />
+        <Dropdown setId={setSideId} options={sideItems} />
+        <Dropdown setId={setDrinkId} options={drinkItems} />
+        <label>Who is this order for? 
+          <OrderNameInput setOrderName={setOrderName} />
+        </label>
+        <OrderImages foodId={foodId} sideId={sideId} drinkId={drinkId} />
+        <InstructionList instructions={instructions} />
+        <InstructionsForm />
       </main>
       <footer>
 
