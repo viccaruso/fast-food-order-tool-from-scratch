069442c425dfe2ec3777129620d93a44f766b78b
Victor Caruso <vic.caruso@gmail.com>
2022-02-02 13:38:49 -0800

    added a few image assets for dropdown selections

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Dropdown({ options, setId }) {
   return (
-    <div>
+    <div className='dropdown'>
       <select onChange={(e) => setId(e.target.value)}>
         {options.map((item, i) => <option key={item + i} value={`${i + 1}`}>{item}</option>)}
       </select>
